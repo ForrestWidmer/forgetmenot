25fff73146e2e42ad73b1d601285808448006196
ForrestWidmer <forrest.widmer@gmail.com>
2014-03-11 20:43:35 -0700

    Making some changes to mailer.

--- app/mailers/prospect_mailer.rb
+++ app/mailers/prospect_mailer.rb
@@ -1,9 +1,9 @@
 class ProspectMailer < ActionMailer::Base
-  default from: "cleartime@website.com"
+  default from: "app@website.com"
 
   def new_email(message)
     @message = message
-    mail(:to => "cleartimetv@mail.com", 
+    mail(:to => "forrest.widmer@gmail.com", 
       :subject => "#{@message.subject}", 
       :body => "#{@message.name} #{@message.company} | #{@message.email} | #{@message.body}" )
   end
