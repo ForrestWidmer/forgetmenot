b3590b933a0fb11a7ea542bab32e8fc7092f0093
ForrestWidmer <forrest.widmer@gmail.com>
2014-04-03 14:24:18 -0700

    Made some changes to the registration forms.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,16 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
-=begin
-  around_filter: :set_user_time_zone
+
+  around_filter :set_time_zone #Dynamically set time zone based on user.time_zone
 
   private
 
   def set_time_zone
     old_time_zone = Time.zone
-    Time.zone = current_user.time_zone if logged_in?
+    Time.zone = current_user.time_zone if user_signed_in?
     yield
   ensure
     Time.zone = old_time_zone
   end
-=end
 end
